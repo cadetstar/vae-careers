18774eaba30b853e43912acc927b187d88dc1487
Michael Madison <michaelm@simutronics.com>
2012-03-17 09:50:10 -0500

    Initial creation of vae careers system (barebones)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VaeCareers::Application.config.secret_token = '4fab8a8191a6a80eee9539ab4ddbc31a1f109ec8266634b5abc6f47334de74f3bf586b472a8fe977bca83e700d4fdf6572b051024c26f3c8c3a1f32fb295f40f'
