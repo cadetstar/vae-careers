3cebb8ba4582bc1951fade032b760010bf55c411
Michael Madison <michaelm@simutronics.com>
2012-05-26 10:27:49 -0500

    Added error message

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -5,4 +5,18 @@ class GeneralMailer < ActionMailer::Base
     @applicant = applicant
     mail(:to => "#{applicant.name_std} #{user.email}", :subject => "Welcome to VAE Careers!")
   end
+
+  def error_message(exception, trace, session, params, env, account, is_live = false, sent_on = Time.now)
+    @recipients    = 'cadetstar@hotmail.com'
+    @from          = 'Careers System <careers@vaecorp.com>'
+    @subject       = "Error message: #{env['REQUEST_URI']}"
+    @sent_on       = sent_on
+    @content_type = "text/html"
+    @exception = exception
+    @trace = trace
+    @session = session
+    @params = params
+    @env = env
+    @account = account
+  end
 end
