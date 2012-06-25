02e101c379c22ea162d14a4855c64bd1b1cb5b08
Michael Madison <michaelm@simutronics.com>
2012-06-25 10:05:37 -0500

    Fixed zip creation

--- app/controllers/dynamic_files_controller.rb
+++ app/controllers/dynamic_files_controller.rb
@@ -5,11 +5,8 @@ class DynamicFilesController < ApplicationController
     files_size = @resource.dynamic_file_revisions.size
     @resource.update_attributes(params[controller_name.singularize.to_sym])
     flash[:notice] = "#{controller_name.singularize.titleize} updated."
-    if @resource.dynamic_file_revisions.reload.size > files_size
-      redirect_to :action => :edit
-    else
-      redirect_to :action => :index
-    end
+
+    redirect_to :action => :edit
   end
 
   def test_compile
